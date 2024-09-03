04c7d14bda628a8597e15a48c6743687e9c312cd
Abdelrady <ahmedmc95@gmail.com.com>
2024-09-03 11:29:03 +0300

    mongez-form-ex

--- src/apps/front-office/design-system/components/Form/TextInputV2.tsx
+++ src/apps/front-office/design-system/components/Form/TextInputV2.tsx
@@ -8,70 +8,77 @@ import {
 import { cn } from "apps/front-office/design-system/utils/cn";
 import { forwardRef } from "react";
 
-function _TextInputV2(props: FormControlProps, ref: any) {
-  const {
-    value,
-    id,
-    error,
-    type,
-    disabled,
-    changeValue,
-    inputRef,
-    otherProps,
-    name,
-  } = useFormControl({
-    ...props,
-    rules: [requiredRule, minLengthRule, lengthRule],
-  });
+type TextInputProps = {
+  children?: React.ReactNode;
+} & FormControlProps;
 
-  return (
-    <div className="w-full">
-      {otherProps.label && (
-        <label
-          htmlFor={id}
-          className="block text-gray-700 cursor-pointer font-medium text-base mb-2">
-          {...otherProps.label}{" "}
-          {props.required && <span className="ms-1 text-red-500">*</span>}
-        </label>
-      )}
+const TextInputV2 = forwardRef<HTMLInputElement, TextInputProps>(
+  function _TextInputV2({ ...props }, ref) {
+    const {
+      value,
+      id,
+      error,
+      type,
+      disabled,
+      changeValue,
+      inputRef,
+      otherProps,
+      name,
+    } = useFormControl({
+      ...props,
+      rules: [requiredRule, minLengthRule, lengthRule],
+      name: "text",
+    });
 
-      <input
-        type={type || "text"}
-        name={name}
-        value={value}
-        id={id}
-        onChange={e => {
-          changeValue(e.target.value);
-        }}
-        ref={_ref => {
-          inputRef.current = _ref;
-          if (ref) {
-            ref.current = _ref;
-          }
-        }}
-        disabled={disabled}
-        {...otherProps}
-        className={cn(
-          "mt-1 block w-full p-2 bg-white border border-border rounded-md text-base placeholder-slate-400 focus:outline-none focus:border-primary-main ",
-          error && "border-l-[2px] border-l-red-500 focus:border-l-red-500",
-          value &&
-            !error &&
-            "border-l-[2px] border-l-green-500 focus:border-l-green-500",
-          otherProps.className,
+    return (
+      <div className="w-full">
+        {otherProps.label && (
+          <label
+            htmlFor={id}
+            className="block text-gray-700 cursor-pointer font-medium text-base mb-2">
+            {otherProps.label}{" "}
+            {props.required && <span className="ms-1 text-red-500">*</span>}
+          </label>
         )}
-      />
 
-      <div
-        className={cn(
-          "text-red-500 text-sm mt-1 transition-all duration-200 overflow-hidden h-0 ",
-          error && "h-[20px]",
-        )}>
-        <span>{error}</span>
+        <input
+          type={type || "text"}
+          name={name}
+          value={value}
+          id={id}
+          onChange={e => {
+            changeValue(e.target.value);
+          }}
+          ref={inputElement => {
+            inputRef.current = inputElement;
+            if (typeof ref === "function") {
+              ref(inputElement);
+            } else if (ref) {
+              ref.current = inputElement;
+            }
+          }}
+          disabled={disabled}
+          {...otherProps}
+          className={cn(
+            "mt-1 block w-full p-2 bg-white border border-border rounded-md text-base placeholder-slate-400 focus:outline-none focus:border-primary-main ",
+            error && "border-l-[2px] border-l-red-500 focus:border-l-red-500",
+            value &&
+              !error &&
+              "border-l-[2px] border-l-green-500 focus:border-l-green-500",
+            otherProps.className,
+          )}
+        />
+
+        <div
+          className={cn(
+            "text-red-500 text-sm mt-1 transition-all duration-200 overflow-hidden h-0 ",
+            error && "h-[20px]",
+          )}>
+          <span>{error}</span>
+        </div>
       </div>
-    </div>
-  );
-}
-
-const TextInputV2 = forwardRef(_TextInputV2);
+    );
+  },
+);
 
 export default TextInputV2;
